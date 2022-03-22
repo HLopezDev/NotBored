ab6b58eedf91ba59e0a2387826d9bd55340c5c35
Hector Orlando Lopez Orozco <hector.lopez@mercadolibre.com.co>
2022-03-21 22:22:24 -0500

    Create getData and deactivate the startButton

--- NotBored/NotBored/HomeViewController.swift
+++ NotBored/NotBored/HomeViewController.swift
@@ -17,24 +17,29 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var participantsTextField: UITextField!
     
+       
     override func viewDidLoad() {
         super.viewDidLoad()
+        participantsTextField.delegate = self
+        startButton.isEnabled = false
+                
         
+    }
 
         // Do any additional setup after loading the view.
+    
+    
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        startButton.isEnabled = participantsTextField.hasText
     }
     
+    
+    
     @IBAction func startButton(_ sender: Any) {
-        self.navigationController?.pushViewController(ActivitiesTableViewController(), animated: true)
-//        Activity.getActivity(completion: { result in
-//            switch result {
-//                case .failure(let error):
-//                    print(error)
-//                case .success(let activity):
-//                    let activity = activity
-//                    print(activity)
-//            }
-//        })
+        let vc = ActivitiesTableViewController()
+        vc.participants = getParticipants()
+        navigationController?.pushViewController(vc, animated: true)
+
     }
     
     @IBAction func termsAndConditionsButton(_ sender: UIButton) {
@@ -42,7 +47,26 @@ class HomeViewController: UIViewController {
         self.present(vc, animated: true)
     }
     
+    func getParticipants() -> Int {
+        let numero = participantsTextField.text ?? "0"
+        return Int(numero) ?? 0
+    }
     
-    
+    func doValidation() {
+        if participantsTextField.hasText {
+            startButton.isEnabled = true
+        }
+    }
 
+}
+
+extension HomeViewController: UITextFieldDelegate {
+// MARK: Restrict textField to only accept numbers.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
+      
+    
 }
